2e8493d089832684398c5c4adb817478cb3ef252
DiegoAraque21 <diego.araque.fernandez@gmail.com>
2022-08-18 09:42:50 -0500

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import './App.css';
+import Papa from 'papaparse';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [users, setUsers] = useState('');
+
+  const changeFile = (e) => {
+    Papa.parse(e.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        setUsers(results.data);
+      },
+    });
+  };
+
+  console.log(users);
+
+  return (
+    <div className="App">
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={changeFile}
+        style={{ display: 'block', margin: '10px auto' }}
+      />
+    </div>
+  );
 }
 
 export default App;
