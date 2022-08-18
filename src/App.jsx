5a392b5a18033adf7fba5a9add3492bf6a3118a0
Marco Torres <marcotorres258@outlook.com>
2022-08-18 09:45:38 -0500

    Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import React from "react";
 import "./App.css";
 
+const rows = [
+  {
+    Index: 1,
+    Name: "Loreto Sepúlveda Torres",
+    ID: "A12345678",
+    Graduation: "24/06/2021",
+    Grade: "83",
+  },
+];
+
 function App() {
+  function getUSFormat(row) {
+    console.log(row);
+
+    const formatedRow = {};
+
+    return formatedRow;
+  }
+
+  React.useEffect(() => {
+    getUSFormat(rows[0]);
+  });
   return <div className="App"></div>;
 }
 
