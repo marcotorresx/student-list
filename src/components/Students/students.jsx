b8c3159a44f198b040cc94a7a921c198c599b516
Marco Torres <marcotorres258@outlook.com>
2022-08-18 17:48:50 -0500

    Add table

--- src/components/Students/students.jsx
+++ src/components/Students/students.jsx
@@ -7,25 +7,28 @@ function Students({ title, data }) {
   return (
     <div className="students reusableCard">
       <h2> {title} </h2>
-      <div className="names reusableCard">
-        <table>
+      <table className="table reusableCard">
+        <thead>
           <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
+            <th>Index</th>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Graduation</th>
+            <th>Grade</th>
           </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-        </table>
-      </div>
+        </thead>
+        <tbody>
+          {data?.map((row) => (
+            <tr>
+              <td>{row.Index}</td>
+              <td>{row.Name}</td>
+              <td>{row.ID}</td>
+              <td>{row.Graduation}</td>
+              <td>{row.Grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
